bdb162b819f8b74935157c81268d6b6c9a11ea06
Antoine Gaulin <antoine.gaulin@polymtl.ca>
2017-03-16 14:37:00 -0400

    Ajout fichiers

---
+++ Objet_celestre.cpp
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \author		Antoine Gaulin
+/// \file		Objet_celestre.h
+/// \date		16/03/17
+/// \brief		Definition de la classe objet celestre
+////////////////////////////////////////////////////////////////////////////////
+
+#include <string>
+#include "Donnee.h"
+
+#ifndef _OBJET_CELESTRE_H_
+#define _OBJET_CELESTRE_H_
+
+using namespace std;
+
+class Objet_celestre
+{
+
+public:
+	
+	enum AscensionDroite{ HEURE, MINUTE, NB_DONNES };
+
+private:
+	Donnee ascensionDroite_[NB_DONNES];
+	Donnee declinaison_;
+	Donnee distance_;
+	Donnee magnitudeApparente_;
+	string nom_;
+};
+
+#endif // _OBJET_CELESTRE_H_
---
+++ Objet_de_messier.cpp
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \author		Antoine Gaulin
+/// \file		Objet_de_Messier.h
+/// \date		16/03/17
+/// \brief		Definition de la classe objet de messier
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _OBJET_MESSIER_H_
+#define _OBJET_MESSIER_H_
+
+using namespace std;
+
+class Objet_messier
+{
+
+public:
+
+private:
+
+};
+
+#endif // _OBJET_MESSIER_H_
