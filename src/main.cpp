087cf154095c78efb7040114a3d0a3cb01ec9e94
Antoine Gaulin <antoine.gaulin@outlook.com>
2021-03-03 22:32:46 -0500

    finalisation du review

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-const int N_OBJECTS = 110;
+const int N_OBJECTS = 110, N_NIGHT_STATES = 3, N_MONTHS = 12;
 
 enum Months { JAN, FEV, MAR, AVR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC };
 enum NightTime { DUSK, NIGHT, MORM };
@@ -31,11 +31,7 @@ struct Star : Coordinate {
 	string name = "", constellation = "";
 };
 
-struct Matrix {
-	double element[3][12];
-};
-
-void readObjects(Object* objects) {
+void readFromDisk(Object* objects) {
 	ifstream file("./data/objects", ios::binary);
 	file.seekg(0, ios::beg);
 	for (int i = 0; i < N_OBJECTS; ++i) {
@@ -50,48 +46,7 @@ void readObjects(Object* objects) {
 	}
 }
 
-void constructMatrix(Matrix& meridian) {
-	for (int i = 0; i < 3; ++i) {
-		meridian.element[DUSK + i][NOV] = 1.5 + 3 * (i + 0);
-		meridian.element[DUSK + i][DEC] = 1.5 + 3 * (i + 0);
-		meridian.element[DUSK + i][JAN] = 1.5 + 3 * (i + 1);
-		meridian.element[DUSK + i][FEV] = 1.5 + 3 * (i + 2);
-		meridian.element[DUSK + i][MAR] = 1.5 + 3 * (i + 2);
-		meridian.element[DUSK + i][AVR] = 1.5 + 3 * (i + 3);
-		meridian.element[DUSK + i][MAY] = 1.5 + 3 * (i + 4);
-		meridian.element[DUSK + i][JUN] = 1.5 + 3 * (i + 4);
-		meridian.element[DUSK + i][JUL] = 1.5 + 3 * (i + 5);
-		meridian.element[DUSK + i][AUG] = 1.5 + 3 * (i + 6);
-		meridian.element[DUSK + i][SEP] = 1.5 + 3 * (i + 6);
-		meridian.element[DUSK + i][OCT] = 1.5 + 3 * (i + 7);
-	}
-}
-
-void checkVisibility(const Matrix& meridian, Object* objects) {
-	time_t now = time(0);
-   	struct tm * date_time = localtime(&now);
-	int month = date_time->tm_mon, hour = date_time->tm_hour;
-
-	int period = -1;
-	if (20 <= hour <= 22)
-		period = DUSK;
-	else if (23 == hour || 0 <= hour <= 1)
-		period = NIGHT;
-	else if (2 <= hour <= 4)
-		period = MORM;
-
-	RightAscension raMin, raMax;
-	raMin.hour = meridian.element[period][month] - 1.5;
-	raMax.hour = meridian.element[period][month] + 1.5;
-
-	for (int i = 0; i < N_OBJECTS; ++i) {
-		bool isVisible = (raMin.hour <= objects[i].rightAscension.hour) && (objects[i].rightAscension.hour <= raMax.hour);
-		if (!isVisible || objects[i].magnitude >= 8)
-			objects[i].isVisible = false;
-	}
-}
-
-void showStar(Star etoile) {
+void printStar(Star etoile) {
 	if (etoile.name != "")
 		cout << endl << "Pour le calibrage, utilisez " << etoile.name <<
 		" dans la constellation " << etoile.constellation << "." << endl
@@ -101,7 +56,7 @@ void showStar(Star etoile) {
 		etoile.declination << endl << endl;
 }
 
-void locateStar(Object* objects) {
+void findStar(Object* objects) {
 	Star star;
 	for (int i = 0; i < N_OBJECTS; ++i) {
 		if (objects[i].isVisible) {
@@ -171,10 +126,27 @@ void locateStar(Object* objects) {
 			}
 		}
 	}
-	showStar(star);
+	printStar(star);
 }
 
-void showResults(Object* objects) {
+void constructMatrix(double meridian[N_NIGHT_STATES][N_MONTHS]) {
+	for (int i = 0; i < N_NIGHT_STATES; ++i) {
+		meridian[DUSK + i][NOV] = 1.5 + 3 * (i + 0);
+		meridian[DUSK + i][DEC] = 1.5 + 3 * (i + 0);
+		meridian[DUSK + i][JAN] = 1.5 + 3 * (i + 1);
+		meridian[DUSK + i][FEV] = 1.5 + 3 * (i + 2);
+		meridian[DUSK + i][MAR] = 1.5 + 3 * (i + 2);
+		meridian[DUSK + i][AVR] = 1.5 + 3 * (i + 3);
+		meridian[DUSK + i][MAY] = 1.5 + 3 * (i + 4);
+		meridian[DUSK + i][JUN] = 1.5 + 3 * (i + 4);
+		meridian[DUSK + i][JUL] = 1.5 + 3 * (i + 5);
+		meridian[DUSK + i][AUG] = 1.5 + 3 * (i + 6);
+		meridian[DUSK + i][SEP] = 1.5 + 3 * (i + 6);
+		meridian[DUSK + i][OCT] = 1.5 + 3 * (i + 7);
+	}
+}
+
+void printResults(Object* objects) {
 	unsigned counter = 0;
 	for (int i = 0; i < N_OBJECTS; ++i) {
 		if (objects[i].isVisible) {
@@ -194,12 +166,36 @@ void showResults(Object* objects) {
 		cout << endl << "Aucun objet Messier n'est visible :(" << endl;
 }
 
+void checkVisibility(const double meridian[N_NIGHT_STATES][N_MONTHS], Object* objects) {
+	time_t now = time(0);
+   	struct tm * date_time = localtime(&now);
+	int month = date_time->tm_mon, hour = date_time->tm_hour;
+
+	int period = -1;
+	if (20 <= hour <= 22)
+		period = DUSK;
+	else if (23 == hour || 0 <= hour <= 1)
+		period = NIGHT;
+	else if (2 <= hour <= 4)
+		period = MORM;
+
+	RightAscension raMin, raMax;
+	raMin.hour = meridian[period][month] - 1.5;
+	raMax.hour = meridian[period][month] + 1.5;
+
+	for (int i = 0; i < N_OBJECTS; ++i) {
+		bool isVisible = (raMin.hour <= objects[i].rightAscension.hour) && (objects[i].rightAscension.hour <= raMax.hour);
+		if (!isVisible || objects[i].magnitude >= 8)
+			objects[i].isVisible = false;
+	}
+	printResults(objects);
+}
+
 int main() {
 	Object objects[N_OBJECTS];
-	readObjects(objects);
-	Matrix meridian;
-	constructMatrix(meridian);
-	checkVisibility(meridian, objects);
-	locateStar(objects);
-	showResults(objects);
+	readFromDisk(objects);
+	findStar(objects);
+	double meridians[N_NIGHT_STATES][N_MONTHS];
+	constructMatrix(meridians);
+	checkVisibility(meridians, objects);
 }
